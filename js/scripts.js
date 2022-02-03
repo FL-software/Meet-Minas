b1020a50c164b2cf91b15c1de26fed3bd61817c1
brunno51 <bruno51_@hotmail.com>
2022-02-03 20:36:39 -0300

    projeto finalizado.

--- js/scripts.js
+++ js/scripts.js
@@ -58,23 +58,45 @@ let _sliderCounter = 0, _touchStart, _touchEnd;
 _elements.date.innerHTML = new Date().getFullYear() + ".";
 
 _elements.scrollLinks.forEach(link => {
+	link.addEventListener("click", e => {
+		_elements.navbarList.classList.remove("navbar-list--show-links");
 
+		const id = link.getAttribute("href");
+		const element = document.querySelector(id);
+
+		const position = element.offsetTop - 62;
+
+		window.scrollTo({
+			top: position,
+			behavior: "smooth"
+		});
+
+		e.preventDefault();
+	});
 });
 
 _elements.toggle.addEventListener("click", () => {
-
+	_elements.navbarList.classList.toggle("navbar-list--show-links");
 });
 
 _elements.galleryItems.forEach(item => {
+	item.addEventListener("click", e => {
+		const id = getImageId(e.target);
+		updateModal(id);
 
+		_elements.modal.style.display = "flex";
+	});
 });
 
 _elements.sliderThumbsImage.forEach(img => {
-
+	img.addEventListener("click", e => {
+		const id = getImageId(e.target);
+		updateModal(id);
+	});
 });
 
 _elements.closeModalBtn.addEventListener("click", () => {
-
+	_elements.modal.style.display = "none";
 });
 
 _elements.sliderNextButton.addEventListener("click", () => nextImage());
@@ -82,19 +104,38 @@ _elements.sliderNextButton.addEventListener("click", () => nextImage());
 _elements.sliderPrevButton.addEventListener("click", () => prevImage());
 
 const getImageId = (target) => {
+	const arrFromChildren = Array.from(target.parentNode.children);
+	const id = arrFromChildren.indexOf(target);
+	
+	_sliderCounter = id;
 
+	return id;
 }
 
 const updateModal = (imgId) => {
+	_elements.sliderImage.src = _gallery[imgId].img;
+	_elements.sliderImageNumber.innerHTML = (imgId + 1) + "/" + _gallery.length;
+	_elements.sliderImageDescription.innerHTML = _gallery[imgId].description;
 
+	_elements.sliderThumbsImage.forEach(img => {
+		img.classList.remove("slider-thumbs__img--active");
+	});
+
+	_elements.sliderThumbsImage[imgId].classList.add("slider-thumbs__img--active");
 }
 
 const nextImage = () => {
-
+	if(++_sliderCounter > 7 ){
+		_sliderCounter = 0;
+	}
+	updateModal(_sliderCounter);
 }
 
 const prevImage = () => {
-
+	if(--_sliderCounter < 0 ){
+		_sliderCounter = _gallery.length - 1;
+	}
+	updateModal(_sliderCounter);
 }
 
 /**************************************************************************/
